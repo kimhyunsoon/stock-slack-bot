6342edaf44e8adea90830d9262acf10cf540d7ec
soon <sudosoon@gmail.com>
2023-08-02 14:37:40 +0900

    edit chart

--- src/schedule/chart.ts
+++ src/schedule/chart.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { scheduleJob } from 'node-schedule';
 import logger from '../debug/logger';
 import ChartJsImage from 'chartjs-to-image';
@@ -16,16 +17,18 @@ interface DatasetlInterface {
   data: number[]
   borderColor: string
   backgroundColor: string
+  yAxisID: string
 }
 
 function chartScheduler(): void {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  scheduleJob('1 7 * * 5', async () => { // utc+9 기준 매주 금요일 16:05에 실행
+  scheduleJob('1 7 * * 5', async () => { // utc+9 기준 매주 금요일 16:01에 실행
     try {
       // 차트 데이터
       const items = await Stock.getList({ itemsPerPage: 0, page: 1 });
       const labels: string[] = [];
       const datasets: DatasetlInterface[] = [];
+      const yAxes: Array<Record<string, unknown>> = [];
       for (let i = 0; i < items.length; i += 1) {
         const { name, logs } = items[i];
         const dataset: DatasetlInterface = {
@@ -33,13 +36,22 @@ function chartScheduler(): void {
           data: [],
           borderColor: colors[i % 5],
           backgroundColor: 'transparent',
+          yAxisID: `y${i}`,
         };
+        yAxes.push({
+          id: `y${i}`,
+          type: 'linear',
+          display: false,
+          position: 'left',
+        });
         for (const log of logs) {
           const { price, created } = log;
           dataset.data.push(Number(price));
           if (labels.length < logs.length) labels.push(time.makeChartDate(created));
         }
-        datasets.push(dataset);
+        datasets.push({
+          ...dataset,
+        });
       }
 
       // 차트 생성
@@ -50,8 +62,49 @@ function chartScheduler(): void {
           labels,
           datasets,
         },
+        options: {
+          scales: {
+            yAxes,
+          },
+          layout: {
+            padding: {
+              left: 40,
+              right: 40,
+            },
+          },
+          plugins: {
+            datalabels: {
+              align: 'top',
+              color: '#fff',
+              font: {
+                weight: 'bold',
+              },
+              backgroundColor: '#00000070',
+              borderRadius: 4,
+              formatter: (value: number) => `₩ ${value.toLocaleString()}`,
+              display: (context: any) => {
+                const { dataset: { data }, dataIndex } = context;
+                let maxIndex = 0;
+                let minIndex = 0;
+
+                for (let i = 1; i < data.length; i++) {
+                  if (data[i] > data[maxIndex]) {
+                    maxIndex = i;
+                  } else if (data[i] < data[minIndex]) {
+                    minIndex = i;
+                  }
+                }
+                if (
+                  dataIndex === maxIndex ||
+                  dataIndex === minIndex
+                ) return true;
+                return false;
+              },
+            },
+          },
+        },
       });
-      chart.setWidth(800).setHeight(400).setBackgroundColor('#fff');
+      chart.setWidth(800).setHeight(600).setBackgroundColor('#fff');
 
       // 파일로 저장
       const outputPath = path.join(__dirname, '../../uploads/chart.png');
